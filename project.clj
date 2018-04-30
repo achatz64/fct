46213c228eb563f4f45aa06910fa7b8ee04d387a
Andre Chatzistamatiou <a.chatzistamatiou@uni-due.de>
2018-04-30 15:43:45 +0200

    new version 0.6.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fct "0.5.2"
+(defproject fct "0.6.0"
   :description "Free variables in Clojure. We provide a framework for global and free variables in Clojure. Global means they have a global meaning (unlike variables in a function declaration) and free means that they are undefined. This turns any code into a function that can be evaluated by assigning interpretations to the variables."
   :url "http://github.com/achatz64/fct"
   :license {:name "Eclipse Public License"
